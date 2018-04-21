dfd8c7d88f01c9ede7a10068d9f8e776f3dc4a62
Matthias Naaf <matthias.naaf@rwth-aachen.de>
2018-04-21 20:51:33 +0200

    WIP: Very first attempt at rewriting acceleration
    Still a lot of code, but hopefully much easier to understand
    (since the different heuristics are performed one after each other).

---
+++ src/strategy/accelerate.cpp
@@ -0,0 +1,467 @@
+/*  This file is part of LoAT.
+ *  Copyright (c) 2015-2016 Matthias Naaf, RWTH Aachen University, Germany
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses>.
+ */
+
+#include "accelerate.h"
+
+#include "preprocess.h"
+#include "accelerate/recurrence.h"
+#include "z3/z3toolbox.h"
+#include "accelerate/farkas.h"
+#include "infinity.h"
+#include "asymptotic/asymptoticbound.h"
+
+#include "its/rule.h"
+
+#include "chaining.h"
+
+#include "debug.h"
+#include "util/stats.h"
+#include "util/timing.h"
+#include "util/timeout.h"
+
+#include <queue>
+
+
+using namespace std;
+
+
+
+Accelerator::Accelerator(LinearITSProblem &its, LocationIdx loc)
+        : its(its), targetLoc(loc) {
+}
+
+
+
+// #####################
+// ##  Preprocessing  ##
+// #####################
+
+
+static void Accelerator::simplifyRule(LinearRule &rule) {
+    Timing::start(Timing::Preprocess);
+
+    // TODO: Port preprocess to Linear/AbstractRule
+//    if (Preprocess::simplifyTransition(itrs,getTransData(tidx))) {
+//        debugGraph("Simplified transition before Farkas");
+//    }
+
+    Timing::done(Timing::Preprocess);
+}
+
+
+static void Accelerator::chainAllLoops(LinearITSProblem &its, LocationIdx loc) {
+    vector<TransIdx> loops = its.getTransitionsFromTo(loc, loc);
+
+    for (TransIdx first : loops) {
+        for (TransIdx second : loops) {
+            if (first == second) {
+                continue;
+            }
+
+            auto chained = Chaining::chainRules(its, its.getRule(first), its.getRule(second));
+            if (chained) {
+                its.addRule(chained.get());
+            }
+        }
+    }
+}
+
+
+
+// #####################################
+// ##  Acceleration, filling members  ##
+// #####################################
+
+
+Accelerator::MeteringResult Accelerator::meter(const LinearRule &rule) const {
+    FarkasTrans t {.guard = rule.getGuard(), .update = rule.getUpdate(), .cost = rule.getCost()};
+
+    MeteringResult res;
+    res.result = FarkasMeterGenerator::generate(its, t, res.meteringFunction, &res.conflictVar);
+
+    // Farkas might modify the rule (e.g. by instantiating free variables)
+    // TODO: Fix this! Might call generate() with mutable guard and constant update (guard has to be appended if reals are allowed)
+    // TODO: The only thing that modifies farkasTrans is the free var instantiation, which could also be done separately!
+    LinearRule newRule(rule.getLhsLoc(), t.guard, t.cost, rule.getRhsLoc(), t.update);
+    res.modifiedRule = newRule;
+
+    return res;
+}
+
+
+bool Accelerator::handleMeteringResult(TransIdx ruleIdx, MeteringResult res) {
+    switch (res.result) {
+        case FarkasMeterGenerator::ConflictVar:
+            rulesWithConflictingVariables.push_back({ruleIdx, res.conflictVar});
+            // fall through, ConflictVar is just UNSAT with additional information
+
+        case FarkasMeterGenerator::Unsat:
+            Stats::add(Stats::SelfloopNoRank);
+            debugAccel("Farkas unsat for rule " << ruleIdx);
+
+            // Maybe the loop is just too difficult for us, so we allow to skip it
+            addEmptyRuleToSkipLoops = true;
+
+            // Maybe we can only find a metering function if we nest this loop with an accelerated inner loop,
+            // or if we try to strengthen the guard
+            outerNestingCandidates.push_back({ruleIdx});
+            rulesWithUnsatMetering.insert(ruleIdx);
+
+            // We cannot accelerate, so we just keep the unaccelerated rule
+            keepRules.insert(ruleIdx);
+            return false;
+
+        case FarkasMeterGenerator::Nonlinear:
+            Stats::add(Stats::SelfloopNoRank);
+            debugAccel("Farkas nonlinear for rule " << ruleIdx);
+
+            // Maybe the loop is just too difficult for us, so we allow to skip it
+            addEmptyRuleToSkipLoops = true;
+
+            // We cannot accelerate, so we just keep the unaccelerated rule
+            keepRules.insert(ruleIdx);
+            return false;
+
+        case FarkasMeterGenerator::Unbounded:
+            Stats::add(Stats::SelfloopInfinite);
+            debugAccel("Farkas unbounded for rule " << ruleIdx);
+
+            // The rule is nonterminating. We can ignore the update, but the guard still has to be kept.
+            {
+                LinearRule newRule = std::move(res.modifiedRule);
+                newRule.getCostMut() = Expression::InfSymbol;
+                newRule.getUpdateMut().clear();
+
+                TransIdx t = its.addRule(std::move(newRule));
+
+                proofout << "  Self-Loop " << ruleIdx << " has unbounded runtime, ";
+                proofout << "resulting in the new transition " << t << "." << endl;
+            }
+            return true;
+
+        case FarkasMeterGenerator::Success:
+            debugAccel("Farkas success, got " << res.meteringFunction << " for rule " << ruleIdx);
+            {
+                LinearRule newRule = std::move(res.modifiedRule);
+                if (!Recurrence::calcIterated(its, newRule, res.meteringFunction)) {
+                    Stats::add(Stats::SelfloopNoUpdate);
+
+                    // Maybe the loop is just too difficult for us, so we allow to skip it
+                    addEmptyRuleToSkipLoops = true;
+
+                    // We cannot accelerate, so we just keep the unaccelerated rule
+                    keepRules.insert(ruleIdx);
+
+                    // Note: We do not add this rule to outerNestingCandidates,
+                    // since it will probably still fail after nesting.
+                    return false;
+
+                } else {
+                    Stats::add(Stats::SelfloopRanked);
+                    TransIdx newIdx = its.addRule(std::move(newRule));
+
+                    // Since acceleration worked, the resulting rule could be an inner loop for nesting
+                    innerNestingCandidates.push_back({ ruleIdx, newIdx });
+
+                    // We also try the original, unaccelerated rule as outer loop for nesting (as in the Unsat case)
+                    outerNestingCandidates.push_back({ ruleIdx });
+
+                    proofout << "  Self-Loop " << ruleIdx << " has the metering function ";
+                    proofout << res.meteringFunction << ", resulting in the new transition ";
+                    proofout << newIdx << "." << endl;
+                    return true;
+                }
+            }
+    }
+    unreachable();
+}
+
+
+bool Accelerator::accelerateAndStore(TransIdx ruleIdx, const LinearRule &rule, bool storeOnlySuccessful) {
+    MeteringResult res = meter(rule);
+
+    if (storeOnlySuccessful
+        && res.result != FarkasMeterGenerator::Unbounded
+        && res.result != FarkasMeterGenerator::Success) {
+        return false;
+    }
+
+    return handleMeteringResult(ruleIdx, res);
+}
+
+
+
+// ########################
+// ##  Nesting of Loops  ##
+// ########################
+
+
+bool Accelerator::canNest(const LinearRule &inner, const LinearRule &outer) const {
+    // Collect all variables appearing in the inner guard
+    ExprSymbolSet innerGuardSyms;
+    for (const Expression &ex : inner.getGuard()) {
+        ex.collectVariables(innerGuardSyms);
+    }
+
+    // If any of these variables is affected by the outer update,
+    // then applying the outer loop can affect the inner loop's condition,
+    // so it might be possible the execute the inner loop again (and thus nesting might work).
+    for (const auto &it : outer.getUpdate()) {
+        ExprSymbol updated = its.getGinacSymbol(it.first);
+        if (innerGuardSyms.count(updated) > 0) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+void Accelerator::addNestedRule(const LinearRule &accelerated, const LinearRule &chain,
+                                    TransIdx inner, TransIdx outer,
+                                    vector<InnerNestingCandidate> &nested) {
+    // Add the new rule
+    TransIdx newTrans = its.addRule(accelerated);
+
+    // Try to use the resulting rule as inner rule again later on
+    // (in case there are actually 3 nested loops)
+    nested.push_back({inner, newTrans});
+
+    // The outer rule was accelerated (after nesting), so we do not need to keep it anymore
+    keepRules.erase(outer);
+
+    proofout << "  Nested parallel self-loops " << outer << " (outer loop) and " << inner;
+    proofout << " (inner loop), resulting in the new transitions: " << newTrans;
+
+    // Try to combine chain and the accelerated loop
+    auto chained = Chaining::chainRules(its, chain, accelerated);
+    if (chained) {
+        TransIdx chainedTrans = its.addRule(chained.get());
+        nested.push_back({inner, chainedTrans});
+        proofout << ", " << chainedTrans;
+    }
+    proofout << "." << endl;
+}
+
+
+
+bool Accelerator::nestRules(const InnerNestingCandidate &inner, const OuterNestingCandidate &outer,
+                            vector<InnerNestingCandidate> &nested) {
+    bool res = false;
+    const LinearRule &innerRule = its.getRule(inner.newRule);
+    const LinearRule &outerRule = its.getRule(outer.oldRule);
+
+    // Avoid nesting a loop with its original transition or itself
+    if (inner.oldRule == outer.oldRule || inner.newRule == outer.oldRule) {
+        return false;
+    }
+
+    // Skip inner loops with constant costs
+    if (innerRule.getCost().getComplexity() == Complexity::Const) {
+        return false;
+    }
+
+    // Check by some heuristic if it makes sense to nest inner and outer
+    if (!canNest(innerRule, outerRule)) {
+        return false;
+    }
+
+    // Try to nest, executing inner loop first
+    auto innerFirst = Chaining::chainRules(its, innerRule, outerRule);
+    if (innerFirst) {
+        auto accelerated = accelerate(innerFirst.get());
+        if (accelerated) {
+            LinearRule newRule = accelerated.get();
+
+            if (newRule.getCost().getComplexity() <= innerRule.getCost().getComplexity()) {
+                res = true;
+
+                // Add the accelerated rule.
+                // Also try to first execute outer once before the accelerated rule.
+                // FIXME: Is this ("outer once before") really effective? Check on benchmark set! Appears to be quite ugly
+                addNestedRule(newRule, outerRule, inner.oldRule, outer.oldRule, nested);
+            }
+        }
+    }
+
+    // Try to nest, executing outer loop first
+    auto outerFirst = Chaining::chainRules(its, outerRule, innerRule);
+    if (outerFirst) {
+        auto accelerated = accelerate(outerFirst.get());
+        if (accelerated) {
+            LinearRule newRule = accelerated.get();
+
+            if (newRule.getCost().getComplexity() <= innerRule.getCost().getComplexity()) {
+                res = true;
+
+                // Add the accelerated rule.
+                // Also try to first execute inner once before the accelerated rule.
+                // FIXME: Is this ("inner once before") really effective? Check on benchmark set! Appears to be quite ugly
+                addNestedRule(newRule, innerRule, inner.oldRule, outer.oldRule, nested);
+            }
+        }
+    }
+
+    return res;
+}
+
+
+
+// #####################
+// ## Main algorithm  ##
+// #####################
+
+
+void Accelerator::run() {
+    // Since we might add accelerated loops, we store the list of loops before acceleration
+    vector<TransIndex> loops = its.getTransitionsFromTo(targetLoc, targetLoc);
+
+    // Try to accelerate all loops
+    for (TransIdx loop : loops) {
+        // don't try to accelerate loops with INF cost
+        if (its.getRule(loop).getCost().isInfty()) {
+            keepRules.insert(loop);
+            continue;
+        }
+
+        accelerateAndStore(loop, its.getRule(loop));
+
+        if (Timeout::soft()) {
+            goto timeout;
+        }
+    }
+
+
+#ifdef FARKAS_HEURISTIC_FOR_MINMAX
+    // Min-Max heuristic (workaround for missing min/max(A,B) support)
+    for (const ConflictVarsCandidate &can : rulesWithConflictingVariables) {
+        VariableIdx A,B;
+        tie(A,B) = can.conflictVars;
+
+        // Add A > B to the guard, try to accelerate
+        LinearRule rule = its.getRule(can.oldRule);
+        rule.getGuardMut().push_back(its.getGinacSymbol(A) > its.getGinacSymbol(B));
+        if (accelerateAndStore(can.oldRule, rule, true)) {
+            debugAccel("MinMax heuristic (A > B) successful with rule: " << rule);
+        }
+
+        // Add A < B to the guard, try to accelerate
+        rule.getGuardMut().pop_back();
+        rule.getGuardMut().push_back(its.getGinacSymbol(A) < its.getGinacSymbol(B));
+        if (accelerateAndStore(can.oldRule, rule, true)) {
+            debugAccel("MinMax heuristic (A < B) successful with rule: " << rule);
+        }
+    }
+#endif
+
+
+#ifdef FARKAS_TRY_ADDITIONAL_GUARD
+    // Guard strengthening heuristic (might help to find a metering function)
+    for (TransIdx loop : rulesWithUnsatMetering) {
+        LinearRule rule = its.getRule(loop);
+
+        if (FarkasMeterGenerator::prepareGuard(its, rule)) {
+            if (accelerateAndStore(loop, rule, true)) {
+                debugAccel("Guard strengthening successful with rule: " << rule);
+            }
+        }
+
+        if (Timeout::soft()) {
+            goto timeout;
+        }
+    }
+#endif
+
+
+    // Nesting
+    for (int i=0; i < NESTING_MAX_ITERATIONS; ++i) {
+        bool changed = false;
+        vector<InnerNestingCandidate> newInnerCandidates;
+
+        // Try to combine previously identified inner and outer candidates via chaining,
+        // then try to accelerate the resulting rule
+        for (const InnerNestingCandidate &inner : innerNestingCandidates) {
+            for (const OuterNestingCandidate &outer : outerNestingCandidates) {
+                if (nestRules(inner, outer, newInnerCandidates)) {
+                    changed = true;
+                }
+
+                if (Timeout::soft()) {
+                    goto timeout;
+                }
+            }
+        }
+        debugAccel("Nested " << newInnerCandidates.size() << " loops");
+
+        if (!changed || Timeout::soft()) {
+            break;
+        }
+
+        // For the next iteration, use the successfully nested loops as inner loops.
+        // This caputes examples where 3 or more loops are nested.
+        innerNestingCandidates.swap(newInnerCandidates);
+    }
+
+
+    // in case of a timeout, we perform no further acceleration, but still delete the old rules
+    timeout:;
+
+    // Remove old rules
+    proofout << "  Removing the self-loops:";
+    for (TransIdx loop : loops) {
+        if (keepRules.count(loop) == 0) {
+            proofout << " " << loop;
+            its.removeRule(loop);
+        }
+    }
+    proofout << "." << endl;
+
+    // Add a dummy rule to simulate the effect of not executing any loop
+    if (addEmptyRuleToSkipLoops) {
+        TransIdx t = its.addRule(LinearRule(targetLoc, {}, Expression(0), targetLoc, {}));
+        proofout << "  Adding an empty self-loop: " << t << "." << endl;
+    }
+
+    // FIXME: Remember to call this after accelerating loops!
+    // removeDuplicateTransitions(getTransFromTo(node,node));
+}
+
+
+
+static bool Accelerator::accelerateSimpleLoops(LinearITSProblem &its, LocationIdx loc) {
+    if (its.getTransitionsFromTo(loc, loc).empty()) {
+        return false;
+    }
+
+    // Preprocessing
+
+#ifdef CHAIN_BEFORE_ACCELERATE
+    Accelerator::chainAllLoops(its, loc);
+#endif
+
+#ifdef SELFLOOPS_ALWAYS_SIMPLIFY
+    for (TransIdx loop : its.getTransitionsFromTo(loc, loc)) {
+        simplifyRule(its.getRuleMut(loop));
+    }
+#endif
+
+    // Accelerate all loops (includes optimizations like nesting)
+
+    Accelerator accel(its, loc);
+    accel.run();
+    return true;
+}
---
+++ src/strategy/accelerate.h
@@ -0,0 +1,202 @@
+/*  This file is part of LoAT.
+ *  Copyright (c) 2015-2016 Matthias Naaf, RWTH Aachen University, Germany
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses>.
+ */
+
+#ifndef ACCELERATE_H
+#define ACCELERATE_H
+
+#include "global.h"
+
+#include "graph/graph.h"
+#include "its/itsproblem.h"
+#include "expr/expression.h"
+
+#include "accelerate/farkas.h"
+
+#include <boost/optional.hpp
+
+
+class Accelerator {
+public:
+    /**
+     * Replaces all simple loops of the given location with accelerated simple loops
+     * by searching for metering functions and iterated costs/updates.
+     *
+     * Also handles nesting and chaining of parallel simple loops (where possible).
+     *
+     * @return true iff the ITS was modified
+     *         (which is always the case if any simple loops were present)
+     */
+    static bool accelerateSimpleLoops(LinearITSProblem &its, LocationIdx loc);
+
+private:
+    // All data returned by FarkasMeterGenerator::generate
+    struct MeteringResult {
+        FarkasMeterGenerator::Result result;
+        Expression meteringFunction;
+        VariablePair conflictVar;
+        LinearRule modifiedRule;
+    };
+
+    // Potential candidate for the inner loop when nesting two loops.
+    // Inner loops are always accelerated loops, so this stores the original and the accelerated rule.
+    struct InnerNestingCandidate {
+        TransIdx oldRule;
+        TransIdx newRule;
+    };
+
+    // Potential candidate for the outer loop when nesting two loops.
+    // Outer loops are always original rules (before acceleration).
+    struct OuterNestingCandidate {
+        TransIdx oldRule;
+    };
+
+    // Potential candidate for the min-max heuristic for conflicting variables.
+    // The heuristic should work around the issue that we do not support min/max(A,B) in metering functions.
+    struct ConflictVarsCandidate {
+        TransIdx oldRule;
+        VariablePair conflictVars;
+    };
+
+private:
+    Accelerator(LinearITSProblem &its, LocationIdx loc);
+
+
+    /**
+     * Helper that calls Preprocess::simplifyRule
+     */
+    static void simplifyRule(LinearRule &rule);
+
+    /**
+     * Helper that chains all pairs of loops at the given location.
+     * Resulting rules are added to the ITS.
+     */
+    static void chainAllLoops(LinearITSProblem &its, LocationIdx loc);
+
+
+    /**
+     * Main function. Tries to accelerate and nest all loops.
+     * This includes heuristics like min-max and guard strengthening.
+     */
+    void run();
+
+
+    /**
+     * Tries to find a metering function for the given rule
+     */
+    MeteringResult meter(const LinearRule &rule) const;
+
+    /**
+     * Processes the result of finding a metering function for the given rule.
+     * If a metering function was found, the iterated cost and update are computed.
+     * If successful, the rule is added to the ITS.
+     *
+     * Depending on the result, the rule is also added to some of the following members:
+     * inner/outerNestingCandidates, rulesWithoutMetering, rulesWithConflictingVariables, keepRules.
+     *
+     * Depending on the result, the following flag is also set:
+     * addEmptyRuleToSkipLoops.
+     *
+     * Note that most of the decisions are heuristics that try to find a suitable balance
+     * between an exploding number of rules and discarding valuable rules.
+     *
+     * @returns true if acceleration was successful or we found an non-terminating loop
+     */
+    bool handleMeteringResult(TransIdx originalRule, MeteringResult meterResult);
+
+    /**
+     * Tries to accelerate the given loop by searching for a metering function
+     * and then calling handleMeteringResult to decide what to do with the result.
+     *
+     * @param rule The rule to accelerate
+     *
+     * @param originalRule The index of the original rule in the ITS from which rule originates.
+     * This index is passed to handleMeteringResults.
+     *
+     * @param storeOnlySuccessful If true, handleMeteringResult is only called
+     * if a metering function was found or a non-terminating loop was recognized.
+     *
+     * @return the return value of handleMeteringResult (or false if not called)
+     */
+    bool accelerateAndStore(TransIdx originalRule, const LinearRule &rule, bool storeOnlySuccessful = false);
+
+    /**
+     * Tries to accelerate the given loop.
+     * If acceleration is successful or we recognize nontermination, the resulting rule is returned.
+     * Otherwise none is returned.
+     *
+     * No members are modified, so the rule is _not_ added to accelerateRules
+     */
+    boost::optional<LinearRule> accelerate(const LinearRule &rule) const;
+
+
+    /**
+     * Helper function that checks with a simple heuristic if the transitions might be nested loops
+     * (this is done to avoid too many nesting attemts, as finding a metering function takes some time).
+     */
+    bool canNest(const LinearRule &inner, const LinearRule &outer) const;
+
+    /**
+     * Adds the given accelerated rule, which resulted from nesting the given inner and outer transitions.
+     * Also tries to chain the given rule `chain` in front of `accelerated` and adds the result (if it works).
+     * Also takes care of the proof output for nesting.
+     */
+    void addNestedRule(const LinearRule &accelerated, const LinearRule &chain, TransIdx inner, TransIdx outer, std::vector<InnerNestingCandidate> &nested);
+
+    /**
+     * Tries to nest the given nesting candidates (i.e., rules).
+     * If successful, the rules are added to acceleratedRules and to the parameter nested.
+     *
+     * Note that several rules can be added in a single call, since different ways of nesting are tested.
+     *
+     * Returns true if nesting was successful.
+     */
+    bool nestRules(const InnerNestingCandidate &inner, const OuterNestingCandidate &outer,
+                   std::vector<InnerNestingCandidate> &nested);
+
+private:
+    // All rules where acceleration failed, but where we want to keep the un-accelerated rule.
+    std::set<TransIdx> keepRules;
+
+    // Rules that are suitable candidates for Nesting
+    std::vector<InnerNestingCandidate> innerNestingCandidates;
+    std::vector<OuterNestingCandidate> outerNestingCandidates;
+
+    // Rules where acceleration failed since no metering function was found (result was Unsat).
+    // We might try to accelerate these rules again with some optimizations (e.g. guard strengthening).
+    std::set<TransIdx> rulesWithUnsatMetering;
+
+    // Rules where acceleration failed since no metering function was found due to conflicting variables.
+    // We might try to accelerate these rules again after applying the min-max heuristic.
+    // That is, if variables A and B are conflicting, we try adding "A < B" or "A > B" to the guard.
+    std::vector<ConflictVarsCandidate> rulesWithConflictingVariables;
+
+    // After acceleration, incoming loops are chained with all acclerated loops.
+    // This assumes that a loop is always executed at least once.
+    // In some cases (heuristic), it might be better to not execute any loop.
+    // To support this, an empty loop is added in the end (parallel to the other accelerated loops),
+    // so chaining with this loop has the same effect as executing none of the accelerated loops.
+    bool addEmptyRuleToSkipLoops = false;
+
+    // The ITS problem. Accelerated rules are added to the ITS immediately,
+    // but no rules are removed until the very end (end of run()).
+    LinearITSProblem &its;
+
+    // The location for which simple loops shall be accelerated.
+    LocationIdx targetLoc;
+};
+
+#endif // ACCELERATE_H
