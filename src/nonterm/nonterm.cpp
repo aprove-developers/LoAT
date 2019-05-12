27b4595226c8f48835e96593c193c7e6ebeac103
Florian Frohn <florian.frohn@mpi-inf.mpg.de>
2019-05-12 12:35:54 +0200

    search for fixpoints

--- src/nonterm/nonterm.cpp
+++ src/nonterm/nonterm.cpp
@@ -8,6 +8,8 @@
 #include "../accelerate/forward.hpp"
 #include "../accelerate/recurrence/dependencyorder.hpp"
 #include "../analysis/chain.hpp"
+#include "../z3/z3solver.hpp"
+#include "../util/relevantvariables.hpp"
 
 namespace nonterm {
 
@@ -26,6 +28,34 @@ namespace nonterm {
             const GiNaC::exmap &up = chained.getUpdate(0).toSubstitution(its);
             if (Z3Toolbox::checkAll({chained.getGuard()}) == z3::sat && Z3Toolbox::isValidImplication(chained.getGuard(), chained.getGuard().subs(up))) {
                 return {{Rule(chained.getLhsLoc(), chained.getGuard(), Expression::NontermSymbol, sink, {}), ForwardAcceleration::SuccessWithRestriction}};
+            }
+        }
+        Z3Context context;
+        Z3Solver solver(context);
+        for (const Expression &e: r.getGuard()) {
+            solver.add(e.toZ3(context));
+        }
+        for (unsigned int i = 0; i < r.getRhss().size(); i++) {
+            if (!r.isLinear()) {
+                solver.push();
+            }
+            const GiNaC::exmap &up = r.getUpdate(i).toSubstitution(its);
+            const ExprSymbolSet &vars = util::RelevantVariables::find(r.getGuard(), {up}, r.getGuard(), its);
+            for (const ExprSymbol &var: vars) {
+                solver.add(Expression(var == var.subs(up)).toZ3(context));
+            }
+            if (solver.check() == z3::sat) {
+                GuardList newGuard(r.getGuard());
+                const z3::model &model = solver.get_model();
+                for (const ExprSymbol &var: vars) {
+                    const z3::expr &z3var = context.getVariable(var).get();
+                    const Expression &pi = Z3Toolbox::getRealFromModel(model, z3var);
+                    newGuard.emplace_back(var == pi);
+                }
+                return {{Rule(r.getLhsLoc(), newGuard, Expression::NontermSymbol, sink, {}), ForwardAcceleration::SuccessWithRestriction}};
+            }
+            if (!r.isLinear()) {
+                solver.pop();
             }
         }
         return {};
