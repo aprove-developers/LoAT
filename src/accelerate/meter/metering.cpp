9ee56a7b79382b498f4692df21e38fe1f502c3dd
Florian Frohn <florian.frohn@mpi-inf.mpg.de>
2020-03-11 00:26:11 +0100

    keep order

--- src/accelerate/meter/metering.cpp
+++ src/accelerate/meter/metering.cpp
@@ -343,7 +343,7 @@ MeteringFinder::Result MeteringFinder::generate(VarMan &varMan, const Rule &rule
     meter.buildLinearConstraints();
 
     // solve constraints for the metering function (without the "GuardPositiveImplication" for now)
-    const BoolExpr &query = meter.genNotGuardImplication() & meter.genUpdateImplications() & meter.genNonTrivial();
+    const BoolExpr &query = meter.genNonTrivial() & meter.genUpdateImplications() & meter.genNotGuardImplication();
     std::unique_ptr<Smt> solver = SmtFactory::modelBuildingSolver(Smt::chooseLogic({query}), varMan, Config::Z3::MeterTimeout);
     solver->add(query);
     Smt::Result smtRes = solver->check();
