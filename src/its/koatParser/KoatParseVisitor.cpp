fb4eaf56e31e96a0816086029aa4954473d61904
Florian Frohn <ffrohn@batman>
2022-06-14 14:41:01 +0200

    rename KoatBaseVisitor

--- src/its/koatParser/KoatParseVisitor.cpp
+++ src/its/koatParser/KoatParseVisitor.cpp
@@ -2,6 +2,6 @@
 // Generated from Koat.g4 by ANTLR 4.7.2
 
 
-#include "KoatBaseVisitor.h"
+#include "KoatParseVisitor.h"
 
 
--- src/its/koatParser/KoatParseVisitor.h
+++ src/its/koatParser/KoatParseVisitor.h
@@ -1,6 +1,3 @@
-
-// Generated from Koat.g4 by ANTLR 4.7.2
-
 #pragma once
 
 
